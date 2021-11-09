f97180c939d0feeb8718c350af84cb0da9f6fc9a
steveoh <sgourley@utah.gov>
2021-11-08 21:54:04 -0700

    fix: add file extension

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { getNewProjects, getProjectsWithComments, getUpcomingProjects } from './queryService';
+import { getNewProjects, getProjectsWithComments, getUpcomingProjects } from './queryService.js';
 import { format } from 'date-fns';
 
 exports.getData = async () => {
